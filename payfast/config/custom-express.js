6b8ccd3c52ac9b857293195960e4929f6f30c675
Douglas Marra <douglasmsi@gmail.com>
2016-08-29 12:06:00 -0300

    Capit. 2

--- payfast/config/custom-express.js
+++ payfast/config/custom-express.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var load = require('express-load');
 
+var bodyParser = require('body-parser');
+
 module.exports = function(){
 
     var app = express();
+
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+
     load('controllers').into(app);
     return app;
 }
--- payfast/controllers/pagamentos.js
+++ payfast/controllers/pagamentos.js
@@ -1,5 +1,12 @@
 module.exports = function(app){
+
   app.get("/pagamentos",function(req,res){
     res.send('Ok');
+  });
+
+  app.post("/pagamentos/pagamento",function(req,res){
+    var pagamento = req.body;
+    console.log(pagamento);
+    res.send('OK');
   });
 }
