3776d16e83a3abe606ad5b09b74e7b77f53e5724
Douglas Marra <douglasmsi@gmail.com>
2016-08-29 14:38:38 -0300

    Validação dos dados

--- payfast/config/custom-express.js
+++ payfast/config/custom-express.js
@@ -2,6 +2,7 @@ var express = require('express');
 var load = require('express-load');
 
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 module.exports = function(){
 
@@ -9,6 +10,7 @@ module.exports = function(){
 
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
+    app.use(expressValidator());
 
     load('controllers').then('persistencia').into(app);
 
--- payfast/controllers/pagamentos.js
+++ payfast/controllers/pagamentos.js
@@ -8,6 +8,18 @@ module.exports = function(app){
 
   app.post("/pagamentos/pagamento",function(req,res){
     var pagamento = req.body;
+
+    req.assert("forma_de_pagamento","Forma de Pagamento é obrigatoria.").notEmpty();
+    req.assert("valor","Valor é obrigatorio e deve ser um decimal").notEmpty().isFloat();
+    req.assert("moeda","Moeda é obrigatória e deve ter 3 caracteres").notEmpty().len(3,3);
+
+    var errors = req.validationErrors();
+    if(errors){
+      console.log("Erros de validação encontrados");
+      res.status(400).send(errors);
+      return;
+    }
+
     console.log('Processando pagamento...');
 
     var connection = app.persistencia.connectionFactory();
@@ -20,7 +32,11 @@ module.exports = function(app){
 
     pagamentoDao.salva(pagamento,function(exception,result){
       console.log('Pagamento criado: '+ result);
-      res.json(pagamento);
+
+      res.location('/pagamentos/pagamento'+ result.insertId);
+      pagamento.id = result.insertId;
+
+      res.status(201).json(pagamento);
     });
 
 
