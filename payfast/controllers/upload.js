ebf9fa42369337514356a6c4546e3024cc2cc16b
Douglas Marra <douglasmsi@gmail.com>
2016-08-30 16:42:05 -0300

    Upload de arquivos

---
+++ payfast/controllers/upload.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+var zlib = require('zlib');
+
+module.exports = function(app){
+  app.post("/upload/gzip",function(req,res){
+    var arquivo - req.headers.filename;
+    console.log('arquivo recebido: '+arquivo);
+
+    req.pipe(zlib.createGunzip())
+      .pipe(fs.createWriteStream("files/"+arquivo))
+      .on('finish',function(){
+        res.writeHead(201,{'Content-type': 'text/plain'});
+        res.end('ok');
+        console.log('arquivo salvo: files/'+arquivo);
+      });
+  });
+
+}
