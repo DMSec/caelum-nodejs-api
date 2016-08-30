aa31560d7c512445c2f863cd916932f70f8567d5
Douglas Marra <douglasmsi@gmail.com>
2016-08-30 15:32:31 -0300

    Alterando configuração do Banco

--- payfast/persistencia/connectionFactory.js
+++ payfast/persistencia/connectionFactory.js
@@ -1,12 +1,34 @@
 var mysql  = require('mysql');
 
 function createDBConnection(){
-	return mysql.createConnection({
-		host: 'localhost',
-		user: 'root',
-		password: '',
-		database: 'payfast'
-	});
+	if (!process.env.NODE_ENV || process.env.node === 'dev') {
+		return mysql.createConnection({
+			host: 'localhost',
+			user: 'root',
+			password: '',
+			database: 'payfast'
+		});
+	}
+
+	if (process.env.NODE_ENV == 'sandbox') {
+		return mysql.createConnection({
+			host: 'localhost',
+			user: 'root',
+			password: '',
+			database: 'payfast_sandbox'
+		});
+	}
+
+	if (process.env.NODE_ENV == 'production') {
+	 var url = process.env.CLEARDB_DATABASE_URL;
+	 var grupos = url.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?/);
+	 return mysql.createConnection({
+		 host:grupos[3],
+		 user:grupos[1],
+		 password:grupos[2],
+		 database:grupos[4]
+	 });
+ }
 }
 
 module.exports = function() {
