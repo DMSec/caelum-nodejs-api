6624350972b471dd278584aacda438afc05c04b7
Douglas Marra <douglasmsi@gmail.com>
2016-08-31 12:04:28 -0300

    Memcache, winston e morgan

--- payfast/config/custom-express.js
+++ payfast/config/custom-express.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var load = require('express-load');
+var morgan = require('morgan');
+var logger = require('../persistencia/logger.js');
 
 var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
@@ -7,6 +9,14 @@ var expressValidator = require('express-validator');
 module.exports = function(){
 
     var app = express();
+
+    app.use(morgan("common",{
+      stream: {
+        write: function(message){
+          logger.info(message)
+        }
+      }
+    }));
 
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
